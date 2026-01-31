d89d86ec822582d1ff498481d32a222046cb804c
minhcamon <nhatminhyt06@gmail.com>
2026-01-31 21:36:37 +0700

    Add Object ParkingSite and its flow

---
+++ PRJ301_ASM_G7/src/java/dal/SiteDAO.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.ParkingSite;
+
+
+/**
+ *
+ * @author Admin
+ */
+public class SiteDAO extends DBContext {
+    
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public SiteDAO() {
+    }
+    
+    public void create() {
+        
+    }
+    
+    public List<ParkingSite> getAll () {
+        List<ParkingSite> resultList = new ArrayList<>();
+        
+        
+        return resultList;
+    } 
+    
+    public void update() {
+        
+    }
+    
+    public void delete() {
+        
+    }
+    
+    public ParkingSite getById() {
+        ParkingSite result = null;
+        
+        
+        return result;
+    }
+    
+    
+}
---
+++ PRJ301_ASM_G7/src/java/model/ParkingSite.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class ParkingSite {
+
+    private String siteId;
+    private String siteName;
+    private String address;
+    private Region region;
+    private Status siteStatus;
+    private User managerId;
+
+    public ParkingSite(String siteId, String siteName, String address, Region region, Status siteStatus, User managerId) {
+        this.siteId = siteId;
+        this.siteName = siteName;
+        this.address = address;
+        this.region = region;
+        this.siteStatus = siteStatus;
+        this.managerId = managerId;
+    }
+
+    public String getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(String siteId) {
+        this.siteId = siteId;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    public Status getSiteStatus() {
+        return siteStatus;
+    }
+
+    public void setSiteStatus(Status siteStatus) {
+        this.siteStatus = siteStatus;
+    }
+
+    public User getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(User managerId) {
+        this.managerId = managerId;
+    }
+    
+    
+    public enum Region {
+        NORTH("Miền Bắc"),
+        MIDDLE("Miền Trung"),
+        SOUTH("Miền Nam");
+
+        private String label;
+
+        Region(String label) {
+            this.label = label;
+        }
+    }
+
+    public enum Status {
+        ACTIVE("Đang hoạt động"),
+        MAINTERNANCE("Bảo trì"),
+        CLOSED("Đóng cửa");
+        
+        private String label;
+
+        private Status(String label) {
+            this.label = label;
+        }
+    }
+
+}
