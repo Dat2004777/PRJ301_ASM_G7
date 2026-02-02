b4f1e9599b8c2e8282f510f49e360bbfca18f677
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-02-02 13:39:03 +0700

    create Account model and RoleEnum to represent for an account

---
+++ PRJ301_ASM_G7/src/java/model/Account.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Account {
+    private int account_id;
+    private String username;
+    private String password;
+    private RoleEnum role;
+    
+    public Account(){}
+    
+    public Account(int account_id, String username,String password, RoleEnum role){
+        this.account_id = account_id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "account_id=" + account_id + ", username=" + username + ", password=" + password + ", role=" + role + '}';
+    }
+    
+    
+}
---
+++ PRJ301_ASM_G7/src/java/model/RoleEnum.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public enum RoleEnum {
+    ADMIN,
+    STAFF,
+    CUSTOMER;
+    
+//    public static RoleEnum from(String value) {
+//        return value == null
+//            ? null
+//            : RoleEnum.valueOf(value.trim().toUpperCase());
+//    }
+}
