44bf75cd87d68fe549ef251a27fc037d981fccd2
Nguyễn Minh Hiếu <hieumastermind@gmail.com>
2026-02-04 00:38:22 +0700

    create CustomerDAO and AccountDAO to query and retrieve data

---
+++ PRJ301_ASM_G7/src/java/dal/CustomerDAO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CustomerDAO extends DBContext {
+
+    public CustomerDAO() {
+    }
+
+    public void insertCustomer(String firstname, String lastname, String phone, String email, int accountID) {
+        String sql
+                = """
+                INSERT INTO Customers(first_name,last_name,phone,email,account_id)
+                VALUES(?,?,?,?,?)
+                """;
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, phone);
+            ps.setString(4, email);
+            ps.setInt(5, accountID);
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Lỗi thêm giá trị Customer");
+        }
+    }
+}
